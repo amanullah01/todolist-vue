d9bd0b07aa68ab6388449c02f642b345c8e77124
Amanullah Aman <amancse.aiub@gmail.com>
2019-07-07 16:37:25 +0600

    router added

---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the todo list app. v1.0.0. It is the part of vue practice</p>
+  </div>
+</template>
---
+++ src/views/Blog.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Blog</h1>
+    <p>Coming soon!!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Blogs"
+};
+</script>
+
+<style scoped>
+</style>
+
